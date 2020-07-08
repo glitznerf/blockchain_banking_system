42a06a3be658052a86fcd7b78c415c7e75285f94
glitznerf <glitznerf@gmail.com>
2020-07-08 18:26:49 +0200

    Bank contract initialization
    Initialize basic variables

--- banking_system.sol
+++ banking_system.sol
@@ -1,1 +1,15 @@
+pragma solidity >=0.6.0;
 
+contract Bank {
+    uint internal fund;                             // fund of bank
+    uint internal lend;                             // amount lend
+    uint public interestRate;
+    address public owner;                           // trusted owner of the contract
+    mapping (address => uint) internal balance;
+    
+    constructor() public { 
+        owner = msg.sender;
+    }
+    
+    // functions: check balance, deposit money in account, withdraw money from account, send money to another account
+}
