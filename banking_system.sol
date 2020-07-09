60dba12a0b522c8a0e84761197c06085d91e0a9b
glitznerf <glitznerf@gmail.com>
2020-07-09 10:14:09 +0200

    Added interest , keeping track of bank funds

--- banking_system.sol
+++ banking_system.sol
@@ -1,11 +1,13 @@
 pragma solidity >=0.6.0;
 
 contract Bank {
-    uint internal fund;                             // fund of bank
-    uint internal lend;                             // amount lend
-    uint public interestRate;
-    address public owner;                           // trusted owner of the contract
-    mapping (address => int) internal balance;
+    uint internal fund = 100;                           // liquid fund of bank
+    uint internal AuM;                                  // bank's assets under management
+    uint internal lend;                                 // amount lend
+    int public interestRate;                            // interest rate per year
+    address public owner;                               // trusted owner of the contract
+    mapping (address => int) internal balance;          // LUT of balances
+    mapping (address => uint) internal lastTransaction; // LUT of last transaction for interest calculation
     
     constructor() public { 
         owner = msg.sender;
@@ -16,13 +18,33 @@ contract Bank {
         return balance[msg.sender];
     }
     
-    function deposit(int amount) public payable {       // deposit money in the current account
-        balance[msg.sender] += amount;
+    function deposit(uint amount) public payable {                   // deposit money in the current account
+        balance[msg.sender] += interest(balance[msg.sender]);    // add interest to account 
+        lastTransaction[msg.sender] = now;
+        balance[msg.sender] += int(amount);
+        AuM += amount;
+        fund -= uint(interest(balance[msg.sender]));
     }
         
-    function withdraw(int amount) public {              // withdraw money from the current account if funds are sufficient
-        require(amount<=balance[msg.sender]); 
-        balance[msg.sender] -= amount;
+    function withdraw(uint amount) public {                          // withdraw money from the current account if funds are sufficient
+        balance[msg.sender] -= interest(balance[msg.sender]);    // add interest to account 
+        fund -= uint(interest(balance[msg.sender]));
+        lastTransaction[msg.sender] = now;
+        require(int(amount)<=balance[msg.sender], "Balance is not sufficient"); 
+        balance[msg.sender] -= int(amount);
+        msg.sender.transfer(amount);
+        AuM -= amount;
     }
     
+    function interest(int amount) internal view returns(int) {  // calculate interest from last transaction to now, assuming monthly interest
+        uint months = (now-lastTransaction[msg.sender])/60/60/24/30;
+        int interest = amount*(1+interestRate/12)**(months) - amount;
+        return interest;
+    }
+    
+    //TODO: function send money to another account within bank
+    //TODO: integrate loan functionality
+    
 }
+
+//TODO: Create instances of banks with class inheritance
