e1d0a8fa711107537e1d94e5870205799dd1be97
Ayush Somani <46413612+AyushSomani001@users.noreply.github.com>
2020-07-10 00:18:30 +0530

    Including all changes

--- Bank2Customer.sol
+++ Bank2Customer.sol
@@ -1,35 +1,63 @@
 pragma solidity ^0.6.11;
 
 contract Bank {
-    int interestRate = 20; //Percent
-    
+    uint private interestRate; //in %
+    address private owner;
+    uint private bankReserve;
+
+    constructor() public {
+        owner = msg.sender;
+        bankReserve = 1000;
+        interestRate = 20;
+    }
+
     struct account_holders {
-        int balance;
+        uint accountBal;
+        uint lendAmount;
+        bool isBank;
     }
-    
+
     mapping(address => account_holders) customers;
-    
-    receive() external payable {
-        int principle = int(msg.value);
-        int amount = principle + principle*interestRate/100;
-        customers[msg.sender].balance += amount;
+
+    function withdraw(uint withdrawAmt) public payable {
+        require(withdrawAmt <= customers[msg.sender].accountBal);
+        customers[msg.sender].accountBal -= withdrawAmt;
+        msg.sender.transfer(withdrawAmt);
+        bankReserve -= withdrawAmt;
     }
-    
-    function lend(int askingAmount) public payable {
-        int leftOverAmount = customers[msg.sender].balance - askingAmount;
-        if(leftOverAmount < 0) {
-            leftOverAmount += leftOverAmount*interestRate/100;
-        }
-        customers[msg.sender].balance = leftOverAmount;
-        msg.sender.transfer(uint(askingAmount));
+
+    function deposit() external payable {
+        uint amount = uint(ufixed(msg.value) * (1 + ufixed(interestRate)/100));
+        customers[msg.sender].accountBal += amount;
+        bankReserve += amount;
     }
-    
-    function getInterestRate() public view returns(int) {
+
+    function lend(uint lendAmt) public payable {
+        uint amount = uint(ufixed(lendAmt) * (1 + ufixed(interestRate/100)));
+        customers[msg.sender].lendAmount += amount;
+        msg.sender.transfer(uint(lendAmt));
+        bankReserve -= lendAmt;
+    }
+
+    function repayLoan() external payable {
+        uint amount = msg.value;
+        customers[msg.sender].lendAmount += amount;
+        bankReserve += amount;
+    }
+
+    function getInterestRate() public view returns(uint) {
         require(interestRate >= 0, "Improper interest rate is set");
         return interestRate;
     }
-    
-    function updateInterestRate(int newInterestRate) public {
+
+    function updateInterestRate(uint newInterestRate) public {
+        require(msg.sender == owner, "You are not allowed to update the interest rate");
         interestRate = newInterestRate;
+    }
+
+    function internalTransfer(address destination, uint amount) external {
+        require(amount <= customers[msg.sender].accountBal, "Balance is not sufficient");
+        customers[msg.sender].accountBal -= amount;
+        customers[destination].accountBal += amount;
     }
 }
