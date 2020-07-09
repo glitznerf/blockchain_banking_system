df4839dffb6da7574650b8585e82f766b51eca51
glitznerf <glitznerf@gmail.com>
2020-07-09 09:09:34 +0200

    Added basic functionality
    getBalance, deposit and withdraw money

--- banking_system.sol
+++ banking_system.sol
@@ -5,11 +5,24 @@ contract Bank {
     uint internal lend;                             // amount lend
     uint public interestRate;
     address public owner;                           // trusted owner of the contract
-    mapping (address => uint) internal balance;
+    mapping (address => int) internal balance;
     
     constructor() public { 
         owner = msg.sender;
     }
     
-    // functions: check balance, deposit money in account, withdraw money from account, send money to another account
+    // Functions: check balance, deposit money in account, withdraw money from account, send money to another account
+    function getBalance() public view returns(int) {    // return the balance of the current account
+        return balance[msg.sender];
+    }
+    
+    function deposit(int amount) public payable {       // deposit money in the current account
+        balance[msg.sender] += amount;
+    }
+        
+    function withdraw(int amount) public {              // withdraw money from the current account if funds are sufficient
+        require(amount<=balance[msg.sender]); 
+        balance[msg.sender] -= amount;
+    }
+    
 }
