64da5963cd9eb9df7242ac01b4b23bf632850377
glitznerf <glitznerf@gmail.com>
2020-07-09 14:15:59 +0200

    Fixed withdrawal, commenting

--- banking_system.sol
+++ banking_system.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.6.0;
 
 // Assumptions:
 //      - interest is only paid for full months without account interaction
+//      - (liquid) fund of the bank comes from independent central bank
 
 contract Bank {
     uint internal fund = 100;                           // liquid fund of bank
@@ -15,35 +16,36 @@ contract Bank {
     constructor() public { 
         owner = msg.sender;
     }
+
     
-    // Functions: check balance, deposit money in account, withdraw money from account, send money to another account
-    function getBalance() public view returns(int) {    // return the balance of the current account
+    function getBalance() public view returns(int) {                    // return the balance of the current account
         return balance[msg.sender];
     }
     
-    function deposit(uint amount) public payable {                  // deposit money in the current account
+    function deposit() external payable {                               // deposit money in the current account
+        uint amount = msg.value;
         int Interest = interest(balance[msg.sender]);
-        balance[msg.sender] += Interest;                            // add interest to account 
-        fund -= uint(Interest);                                     // subtract interest from bank's funds
+        balance[msg.sender] += Interest;                                // add interest to account 
+        fund -= uint(Interest);                                         // subtract interest from bank's funds
         lastTransaction[msg.sender] = now;
-        balance[msg.sender] += int(amount);
-        AuM += amount;
+        balance[msg.sender] += int(amount);                             // increase account balance
+        AuM += amount;                                                  // increase assets under bank's management
     }
-        
-    function withdraw(uint amount) public {                         // withdraw money from the current account if funds are sufficient
+
+    function withdraw(uint amount) external {                           // withdraw money from the current account if funds are sufficient
         int Interest = interest(balance[msg.sender]);
-        balance[msg.sender] += Interest;                            // add interest to account 
-        fund -= uint(Interest);                                     // subtract interest from bank's funds
+        balance[msg.sender] += Interest;                                // add interest to account 
+        fund -= uint(Interest);                                         // subtract interest from bank's funds
         lastTransaction[msg.sender] = now;
         require(int(amount)<=balance[msg.sender], "Balance is not sufficient"); 
-        balance[msg.sender] -= int(amount);
-        AuM -= amount;
+        balance[msg.sender] -= int(amount);                             // decrease account balance
+        AuM -= amount;                                                  // decrease assets under bank's management
         msg.sender.transfer(amount);
     }
     
-    function interest(int amount) internal view returns(int) {  // calculate interest from last transaction to now, assuming monthly interest
-        uint months = (now-lastTransaction[msg.sender])/60/60/24/30;
-        int Interest = amount*(1+interestRate/12)**(months) - amount;
+    function interest(int amount) internal view returns(int) {          // calculate interest from last transaction to now, assuming monthly interest
+        uint months = (now-lastTransaction[msg.sender])/60/60/24/30;    // number of full months since last account interaction
+        int Interest = amount*(1+interestRate/12)**(months) - amount;   // compound interest gained
         return Interest;
     }
     
